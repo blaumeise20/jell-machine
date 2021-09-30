273484b3d981a0ec54b6d6090879e0e86a4132fb
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-09-30 18:56:10 +0200

    Remove unused stuff

--- src/renderer/core/grid.ts
+++ src/renderer/core/grid.ts
@@ -14,8 +14,6 @@ import arr from "create-arr";
 import { doStep } from "./cellUpdates";
 
 export const openLevel: Writable<CellGrid | null> = writable(null);
-
-openLevel.subscribe(o => (window as any).openLevel = o);
 // export const openLevel2: Writable<CellGrid | null> = writable(null);
 
 // export const activeLevel: Writable<0 | 1 | 2> = writable(0);
