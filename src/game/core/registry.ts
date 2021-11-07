f22b40785ad64cd90687f6d34044b2e8478883bb
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2021-11-07 18:43:34 +0100

    Change registry to namespace

--- src/game/core/registry.ts
+++ src/game/core/registry.ts
@@ -3,41 +3,39 @@ import type { ItemIdentifier } from "./extensions";
 import type { LevelCode } from "./levelCode";
 import type { Slot } from "./slot";
 
-export class Registry {
-    static cells: Record<string, CellType> = {};
-    static slots: Slot[] = [];
-    static levelCodes: Record<string, LevelCode> = {};
+export namespace Registry {
+    const cells: Record<string, CellType> = {};
+    const slots: Slot[] = [];
+    const levelCodes: Record<string, LevelCode> = {};
 
-
-    private constructor() {}
 
     // cell types
-    static getCell(id: ItemIdentifier): CellType | undefined {
-        return this.cells[id];
+    export function getCell(id: ItemIdentifier): CellType | undefined {
+        return cells[id];
     }
-    static getCells(): CellType[] {
-        return Object.values(this.cells);
+    export function getCells(): CellType[] {
+        return Object.values(cells);
     }
-    static registerCell(cellType: CellType) {
-        this.cells[cellType.id] = cellType;
+    export function registerCell(cellType: CellType) {
+        cells[cellType.id] = cellType;
     }
 
     // slots
-    static getSlots() {
-        return this.slots;
+    export function getSlots() {
+        return slots;
     }
-    static registerSlot(slot: Slot) {
-        this.slots.push(slot);
+    export function registerSlot(slot: Slot) {
+        slots.push(slot);
     }
 
     // level codes
-    static registerLevelCode(levelCode: LevelCode) {
-        this.levelCodes[levelCode.id] = levelCode;
+    export function registerLevelCode(levelCode: LevelCode) {
+        levelCodes[levelCode.id] = levelCode;
     }
-    static getLevelCode(id: string): LevelCode | undefined {
-        return this.levelCodes[id];
+    export function getLevelCode(id: string): LevelCode | undefined {
+        return levelCodes[id];
     }
-    static getLevelCodes(): LevelCode[] {
-        return Object.values(this.levelCodes);
+    export function getLevelCodes(): LevelCode[] {
+        return Object.values(levelCodes);
     }
 }
