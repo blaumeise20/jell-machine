23d735592f5e8c44bcf55d16ff85624e65d00c75
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2022-07-08 19:00:24 +0200

    Make animations enabled by default

--- src/game/utils/config.ts
+++ src/game/utils/config.ts
@@ -24,7 +24,7 @@ function write(config: Config) {
 
 class ConfigManager {
     static instance: ConfigManager;
-    static default = { texturePack: false as string | false, tickSpeed: 200, uiScale: 1, animation: false, showDebug: false, showBackgroundGrid: true };
+    static default = { texturePack: false as string | false, tickSpeed: 200, uiScale: 1, animation: true, showDebug: false, showBackgroundGrid: true };
 
     constructor() { return ConfigManager.instance ?? (this.init(), ConfigManager.instance = this) }
 
